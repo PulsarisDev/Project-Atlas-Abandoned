c913beedbf40dee647d2fd9fb56b2e10127732af
PixelGalaxy <wushuo612@163.com>
2018-11-16 15:32:44 +0800

    周常更新 / Weekly update pre_18w46a

--- Project-Atlas/app/views.py
+++ Project-Atlas/app/views.py
@@ -1,12 +1,41 @@
-#region
+#region import
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
+from PIL import Image
 import configparser
+import hashlib
 #endregion
 
-print("0000 Performing system check...")
+#region loadConfig
+print("Loading configurations...")
+try:
+    config = configparser.ConfigParser()
+    config.readfp(open('config.ini'))
+#endregion
+
+#region OperateFunction
+def GetProfilePictureHash(Filename):
+    byte = bytes()
+    hash = hashlib.sha256()
+    img = Image.open('./{}.png'.format(Filename))
+    Width,Height = img.size
+    byte += struct.pack('>1',Width)
+    byte += struct.pack('>1',Height)
+    for x in range(Width):
+        for y in range(Height):
+            tmp = img.getdata()[y*Width+x]
+            if tmp[3] == 0x00:
+                byte += struct.pack('>I',0)
+            else:
+                byte += struct.pack('>B',tmp[3])
+                byte += struct.pack('>B',tmp[0])
+                byte += struct.pack('>B',tmp[1])
+                byte += struct.pack('>B',tmp[2])
+    hash.update(byte)
+    return hash.hexdigest()
+#endregion
 
 def home(request):
     """Renders the home page."""
